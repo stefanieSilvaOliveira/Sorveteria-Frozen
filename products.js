8429888afdd3900e86d4c07682c2124e8710c26b
bonecahStefanie <stefaniesilvadeoliveira@yahoo.com.br>
2023-11-03 14:42:30 -0300

    Produtos-JavaScript

---
+++ products.js
@@ -0,0 +1,6 @@
+const menuOptions = [
+    { name: 'GELATO ROSA', price: 20, src: './img/sorvete-flor-frozen.png' },
+    { name: 'GELATO MORAN', price:  27, src: './img/sorvete-rosa-frozen.png' },
+    { name: 'GELATO FLOCOS', price: 23, vegan: false, src: '/img/sorvete-flor-frozen.png' },
+    { name: 'GELATO FROZEN', price: 28, vegan: false, src: './img/sorvete-rosa-frozen.png' },
+    ]
